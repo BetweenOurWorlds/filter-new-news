c0ecfad62642de035c6ef891235ddea67303c68f
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2023-06-02 09:00:12 +0200

    Add parameter for result to file

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ import {program} from 'commander';
 
 program
   .requiredOption('-o, --old <file>')
-  .requiredOption('-n, --new <file>');
+  .requiredOption('-n, --new <file>')
+  .requiredOption('-r, --result <file>');
 
 program.parse();
 
 const options = program.opts();
 
-main({oldFile: options.old, newFile: options.new});
+main({oldFile: options.old, newFile: options.new, resultFile: options.result});
 
 async function main(options) {
-  const {oldFile, newFile} = options;
+  const {oldFile, newFile, resultFile} = options;
   const oldNews = await fs.readJson(oldFile);
   const newNews = await fs.readJson(newFile);
   const oldLinks = oldNews.map(a => a.link);
 
   const result = newNews.filter(item => !oldLinks.includes(item.link));
-  console.log(result);
+  fs.writeJson(resultFile, result);
 }
